842dda4453fc99d3f12eef28737bbf39a285429f
Frank R. Leotta III <leottaif@go.stockton.edu>
2023-09-26 16:21:02 -0400

    messing around with  commands

--- Test_HW_dimaond.py
+++ Test_HW_dimaond.py
@@ -1,2 +1,16 @@
 ##testing out HW 1 dimaond problem
+s = input("Before you go,I would now like to draw a diamond for you.  Please enter a symbol you would like to use: ")
 
+print("    " + s + "     |")
+print("   " + s + s + s + "    |")
+print("  "+s + s + s + s + s + "   |")
+print(" "+ s + s + s + s + s + s + s + "  |")
+print("  "+ s + s + s + s + s + "   |")
+print("   " + s + s + s + "    |")
+print("    "+ s + "     |")
+## trying it out a new way
+print("    " + s , end = "     |",)
+print("   " + s + s + s , end = "    |")
+
+n = "  "
+##ya no that would not work, since the amount of spaces are different and its in the border
