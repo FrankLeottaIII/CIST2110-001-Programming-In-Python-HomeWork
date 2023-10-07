fedaeb694e12020fa5564a0263ba7c656b453f43
Frank R. Leotta III <leottaif@go.stockton.edu>
2023-10-07 12:06:06 -0400

    questiones coded: Print changed,

--- Project1Folder/Project1_no_Funtions.py
+++ Project1Folder/Project1_no_Funtions.py
@@ -85,9 +85,100 @@ answer: D
 
 """
 
+
 print("Hello, welcome to my quiz game.")
 print(" I will be asking you a few questions, and keeping score, so try your best!")
 print("please enter the letter of the answer you think is correct, or it will regester as incorrect.")
 # Implement at least 5 questions, each with 4 answer options (a, b, c, d). Each question should be worth 1 point.
 
-Print("Question 1")
+print("Question 1")
+print(" Which tree is part of the red oak subspecies?")
+print("A. white oak")  
+print("B. black oak")
+print("C. red maple")
+print("D. chestnut oak")
+
+Answer= D
+
+print("Question 2")
+print(" Which tree is part of the white oak subspecies?")
+print("A. post oak")
+print("B. scarlet oak")
+print("C. silver maple")
+print("D. willow oak")
+
+Answer= A
+
+print("Question 3")
+print(" In ecological silviculture, what is NOT true about the selection system?")
+print("A. it is a silvicultural system that maintains uneven aged stands.")
+print("B. Often represented by “reverse J” dia. distrib. or something slightly more irregular")
+print("C. It is a silvicultural system that maintains even aged stands.")
+print("D. It is a silvicultural system that includes 3 or more age classes of trees.")
+
+Answer= C
+
+print("Question 4")
+print(" what is true about the B-D-q method?")
+print("A.  it provides a guiding curve approach")
+print("B.  applies to group selection")
+print("C.  applies to single tree selection")
+print("D.  it uses a ratio of small tree to large trees in its calculations")
+
+Answer= B
+
+print("Question 5")
+print(" What does the D stand for in the B-D-q method?")
+print("A.  diameter")
+print("B.  Largest density class in the residual stand")
+print("C.  Largest diameter class in the residual stand")
+print("D.  diameter ratio")
+
+Answer= C
+
+print("Question 6")
+print(" what is FALSE about silviculture?")
+print("A.  the theory and practice of influincing forest devilopment, composition, and health to meet broad sociatal needs and objectives")
+print("B.  applied forest ecology")
+print("C.  it is consiterd a form of art")
+print("D.  the theory and practice of studying individuals trees in an urban environment")
+
+Answer= D
+
+print("Question 7")
+print(" what is NOT consitered a multi corhort silvicultural system?")
+print("A.  seed-tree")
+print("B.  selection system")
+print("C.  shelterwood")
+print("D.  clearcut")
+
+Answer= D
+
+print("Question 8")
+print(" What is the correct treatment order for both seed-tree and shelterwood silvicultural systems?")
+print("A. Removal cutting, Prepatory treatment, Establishment cutting,")
+print("B. Prepatory treatment, Establishment cutting, removal cutting, ")
+print("C. Removal cutting, Establishment cutting, Prepatory treatment,")
+print("D. None of the above")
+
+Answer= B
+
+print("Question 9")
+print(" What is consitered a Regeneration method in silviculture?")
+print("A. Site preparation")
+print("B. Release Treatments")
+print("C. timber stand improvement")
+print("D. all of the above")
+
+Answer= A
+
+print("Question 10")
+print(" What NOT correct about the Size density relationship in Silviculture?")
+print("A. Average tree size changes predictably with changes in stand density when stands are growing at max. density")
+print("B. There is Intense competition & density-dependent mortality (self-thinning)in young Forest stands")
+print("C. It uses the Density Management Diagram (DMD)")
+print("D. It uses the Stocking chart Diagram")
+
+Answer= D
+
+
