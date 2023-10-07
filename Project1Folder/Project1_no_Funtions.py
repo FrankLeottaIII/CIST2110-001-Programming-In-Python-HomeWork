498b431651ca0052571a6bfa9d5346a65523f869
Frank R. Leotta III <leottaif@go.stockton.edu>
2023-10-07 11:50:10 -0400

    Reference questions notes

---
+++ Project1Folder/Project1_no_Funtions.py
@@ -0,0 +1,93 @@
+"""
+1.) Which tree is part of the red oak subspecies?
+A. white oak  
+B. black oak
+C. red maple
+D. chestnut oak
+
+Answer: D
+
+2.) Which tree is part of the white oak subspecies?
+A. post oak
+B. scarlet oak
+C. silver maple
+D. willow oak
+
+Answer: A
+
+3.) In ecological silviculture, what is NOT true about the selection system?
+
+A. it is a silvicultural system that maintains uneven aged stands.
+B. Often represented by “reverse J” dia. distrib. or 
+something slightly more irregular
+C. It is a silvicultural system that maintains even aged stands.
+D. It is a silvicultural system that includes 3 or more age classes of trees.
+
+Answer: C
+
+4.) what is true about the B-D-q method?
+A.  it provides a guiding curve approach
+B.  applies to group selection
+C.  applies to single tree selection
+D.  it uses a ratio of small tree to large trees in its calculations
+
+Answer: B
+
+5.) What does the D stand for in the B-D-q method?
+A.  diameter
+B.  Largest density class in the residual stand
+C.  Largest diameter class in the residual stand
+D.  diameter ratio
+
+Answer: C
+
+6.) what is FALSE about silviculture?
+A.  the theory and practice of influincing forest devilopment, composition, and health to meet broad sociatal needs and objectives
+B.  applied forest ecology
+C.  it is consiterd a form of art
+D.  the theory and practice of studying individuals trees in an urban environment
+
+Answer: D
+
+7.)what is NOT consitered a multi corhort silvicultural system?
+A.  seed-tree
+B.  selection system
+C.  shelterwood
+D.  clearcut
+
+Answer: D
+
+8.) What is the correct treatment order for both seed-tree and shelterwood silvicultural systems?
+A. Removal cutting, Prepatory treatment, Establishment cutting,
+B. Prepatory treatment, Establishment cutting, removal cutting, 
+C. Removal cutting, Establishment cutting, Prepatory treatment,
+D. None of the above
+
+Answer: B
+
+9.) What is consitered a Regeneration method in silviculture?
+A. Site preparation
+B. Release Treatments
+C. timber stand improvement
+D. all of the above
+
+Answer: A
+
+10.) What NOT correct about the Size density relationship in Silviculture?
+A.) Average tree size changes predictably with changes in stand density when stands are growing at max. density
+B. There is Intense competition & density-dependent mortality (self-thinning)in young Forest stands
+C. It uses the Density Management Diagram (DMD)
+D. It uses the Stocking chart Diagram
+
+answer: D
+
+## all these questions are for the Ecological Forest Management quiz 2 study guide, and for my programming class as well,
+
+"""
+
+print("Hello, welcome to my quiz game.")
+print(" I will be asking you a few questions, and keeping score, so try your best!")
+print("please enter the letter of the answer you think is correct, or it will regester as incorrect.")
+# Implement at least 5 questions, each with 4 answer options (a, b, c, d). Each question should be worth 1 point.
+
+Print("Question 1")
