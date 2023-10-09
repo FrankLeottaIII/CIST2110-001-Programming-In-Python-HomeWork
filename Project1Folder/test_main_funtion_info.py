0bdfe8abafc938bcea4771147a1b0a1d1b16230a
Frank R. Leotta III <leottaif@go.stockton.edu>
2023-10-09 19:47:08 -0400

    main funtion testing start

---
+++ Project1Folder/test_main_funtion_info.py
@@ -0,0 +1,8 @@
+### Testing main funtion infor
+
+##  import barrel_monkey()  ##invalid syntax
+#  import __barrel_monkey__()  ##invalid syntax
+#  import barrel_monkeys.py  ## ModuleNotFoundError: No module named 'barrel_monkeys'
+## import barrel_monkeys.main  ## ModuleNotFoundError: No module named 'barrel_monkeys'
+##import Main_funtion_info.barrel_monkeys ## ModuleNotFoundError: No module named 'Main_funtion_info.barrel_monkeys'; 'Main_funtion_info' is not a package  
+import 
