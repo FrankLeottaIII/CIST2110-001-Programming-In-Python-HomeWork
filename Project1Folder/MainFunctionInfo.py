56e6de57dde54fe976fd593408a839e46acd8969
Frank R. Leotta III <leottaif@go.stockton.edu>
2023-10-09 20:09:58 -0400

    fixed  py names,, Listed errors, import works

--- Project1Folder/MainFunctionInfo.py
+++ Project1Folder/MainFunctionInfo.py
@@ -2,18 +2,18 @@
 
 global monkeys
 monkeys = 5
-def barrel_monkeys(varible):
+def barrelMonkeys(varible):
     global monkeys
     if monkeys == 1:
         print("1 monkey in the barrel")
     else:
         print(monkeys, "monkeys in the barrel")
         monkeys -= 1
-        barrel_monkeys(monkeys)
+        barrelMonkeys(monkeys)
     return monkeys
 
 
 def main():
     if __name__ == "__main__":
-        barrel_monkeys(monkeys)
+        barrelMonkeys(monkeys)
         main()
---
+++ Project1Folder/TestMainFunction_info.py
@@ -0,0 +1,24 @@
+### Testing main funtion infor
+monkey = 1
+##  import barrel_monkey()  ##invalid syntax
+#  import __barrel_monkey__()  ##invalid syntax
+#  import barrel_monkeys.py  ## ModuleNotFoundError: No module named 'barrel_monkeys'
+## import barrel_monkeys.main  ## ModuleNotFoundError: No module named 'barrel_monkeys'
+##import Main_function_info.barrel_monkeys ## ModuleNotFoundError: No module named 'Main_funtion_info.barrel_monkeys'; 'Main_funtion_info' is not a package  
+#  import MainFunctionInfo.barrelMonkeys(monkey)  ## invalid syntax
+
+## i can import the file, but not the function
+#  import MainFunctionInfo  ## this works
+## import MainFunctionInfo as mfi  ## this works
+import MainFunctionInfo
+MainFunctionInfo.barrelMonkeys(monkey)  ## this works
+## barrelMonkeys(monkey)  ## NameError: name 'barrelMonkeys' is not defined
+
+""" Remember to import it as a module, and then call the function
+I was trying to call the function directly, which is not how it works
+
+Also remember to get rid of snakes in the name, since that can cause problems
+and it will not work
+
+So just use camel case for final product, and snake case for testing
+"""
