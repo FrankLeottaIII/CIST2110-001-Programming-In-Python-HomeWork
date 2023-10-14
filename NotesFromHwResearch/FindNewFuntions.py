7f59bb5bd054d0432b2d624d6e41ecbc15f4ff47
Frank R. Leotta III <leottaif@go.stockton.edu>
2023-10-13 23:16:21 -0400

    find new funtions.py added... for resarch into hw

---
+++ NotesFromHwResearch/FindNewFuntions.py
@@ -0,0 +1,10 @@
+## Finding new funtions in python with copilot
+## https://www.youtube.com/watch?v=9M4XKi25I2M
+
+# list of functions
+# https://docs.python.org/3/library/functions.html #holy  moly, this is so useful... but time consuming to read through
+
+# This is for hw3
+# len(s)
+# Return the length (the number of items) of an object. 
+#The argument may be a sequence (such as a string, bytes, tuple, list, or range) or a collection (such as a dictionary, set, or frozen set).
