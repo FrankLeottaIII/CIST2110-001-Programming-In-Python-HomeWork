a655a16c9632d9adbb05d1840392f2215265f4f6
Frank R. Leotta III <leottaif@go.stockton.edu>
2023-11-14 18:08:15 -0500

    homework doing

--- HW8.py
+++ HW8.py
@@ -4,6 +4,37 @@
 # This homework will exapnd upon the code for Lab9.py. If you did not complete Lab9.py, you should do so before attempting this homework.
 
 # Copy the code from Lab9.py into this file. I'll add some comments to help you out.
+with  open('frankenstein.txt', 'r', encoding='utf-8') as f:
+    # Read the file
+    text = f.read()
+def remove_gutenberg_header_footer(text):
+    start = text.find('*** START OF THE PROJECT GUTENBERG EBOOK FRANKENSTEIN; OR, THE MODERN PROMETHEUS ***')
+    end = text.find('*** END OF THE PROJECT GUTENBERG EBOOK FRANKENSTEIN; OR, THE MODERN PROMETHEUS ***')
+    return text[start:end]
+# Call the remove_gutenberg_header_footer method with the text and save the result to a variable called text
+fixed_text = remove_gutenberg_header_footer(text)
+#printed it out, it worked
+print(fixed_text)
+words_cleaned = fixed_text.replace('.','').replace(',','').replace('!','').replace('?','').replace(':','').replace(';','').replace('***','').replace('/','').replace('_','').replace('(','').replace(')','').replace('-','').replace('--','').replace('"','').lower().split()
+unique_words_cleaned = set(words_cleaned)
+print(unique_words_cleaned)
+print("Number of unique words in the text: ", len(unique_words_cleaned))
+print("number of times monster appears in the text: ", words_cleaned.count('monster'))
+from collections import Counter
+word_counts = Counter(words_cleaned)
+#print(word_counts)
+print("these are the most common words, with their number in the text: ", word_counts.most_common(10))
+
+# Import statements (activate venv and install streamlit if you haven't already)
+# from typing import Text
+# from numpy import append
+# from numpy.core.fromnumeric import size
+# from numpy.lib.function_base import append
+# from numpy.lib.npyio import load
+# from numpy.lib.type_check import imag
+# from numpy.ma.core import append
+# from numpy.ma.extras import append
+# from numpy.ma.tests.test_extras import append
 
 # Import statements (activate venv and install streamlit if you haven't already)
 import streamlit as st
