d3baa63d343533c58a0989e6931e49b3a051af40
Frank R. Leotta III <leottaif@go.stockton.edu>
2023-12-15 15:24:01 -0500

    middle name added for uniformity

--- Project3Folder/Project3Tests.py
+++ Project3Folder/Project3Tests.py
@@ -9,7 +9,7 @@ from Project3 import Book, User, Library
 #   \ \  \___|\ \  \\  \\ \  \\\  \|\  \\_\  \ \  \_|\ \ \  \____   \ \  \          __\_\  \
 #    \ \__\    \ \__\\ _\\ \_______\ \________\ \_______\ \_______\  \ \__\        |\_______\
 #     \|__|     \|__|\|__|\|_______|\|________|\|_______|\|_______|   \|__|        \|_______|
-# Author: Frank R. Leotta III
+# Author: Frank Robert Leotta III
 # CIST2110-001-Project-3 Library Management System (LMS) Test Cases
 
 
